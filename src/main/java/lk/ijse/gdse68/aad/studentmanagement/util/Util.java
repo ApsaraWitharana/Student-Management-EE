53e40e38df77fb508b296e776bb9ffbac9045732
ApsaraWitharana <asachini095@gmail.com>
2024-07-07 15:11:26 +0530

    update-object binding of the json -update

--- src/main/java/lk/ijse/gdse68/aad/studentmanagement/util/Util.java
+++ src/main/java/lk/ijse/gdse68/aad/studentmanagement/util/Util.java
@@ -4,7 +4,7 @@ import java.util.UUID;
 
 
 public class Util {
-
+   //id ek generete krgnnwa uuid type eke id eke return krnwa static hinda
     public static String IdGenerate(){
         return UUID.randomUUID().toString();
     }
