41b6c6c7fdbdba34a86f25ac56f0e9c9cec235dc
ApsaraWitharana <asachini095@gmail.com>
2024-07-21 09:32:16 +0530

    update-21/07/2024 lastweek practrical theory

--- src/main/java/lk/ijse/gdse68/aad/studentmanagement/script.js
+++ src/main/java/lk/ijse/gdse68/aad/studentmanagement/script.js
@@ -49,16 +49,17 @@ function saveStudentData() {
     //05.send object
 
     const http = new XMLHttpRequest() //01
-    http.onreadystatechange = ()=>{
-        //AJAX - ready state ek athulen blnn puluwan eke swabawaya step 5k wenkm ynwa (0-4)
+    http.onreadystatechange = ()=>{ // coll back funshion => ready state ek chak krnn use wenne req ekt dunn resp ek mokkd kiyl blagnn //parameter ekk widiyt data tranceper krnwa (promise method  ekk tiye)
+        //AJAX - ready state ek athulen blnn puluwan eke swabawaya step 5k wenkm ynwa (0-4) //AJAX KIYNNE=> Acetonice and java xml
+        // page ek refresh nokara client ta eyage user experience ek awlk nowi data transper krgnn AJAX use krnne
         // Holds the status of the XMLHttpRequest.
-        // 0: request not initialized - open method not in working
+        // 0: request not initialized - open method not in working //un authorise
         // 1: server connection established - coll the open method and initialized
         // 2: request received - send the req to server // the req wen to sever and get the data //header ek read krl dataa gnn puluwan// paylord eke data tik send wela
         // 3: processing request - req processing but not given complete request property to responseText
         // 4: request finished and response is ready - ready server to get resp
         // we are checking statement 4 because we can get the result to statement 4
-        //
+        //readystate => req ek t mokd une rsp ekkk awad kiyl dn gnn
         // status
         // 200: "OK"
         // 403: "Forbidden"
@@ -68,10 +69,10 @@ function saveStudentData() {
         if (http.readyState == 4){
             if (http.status == 200){
                // property godak tiyenwa api dunn req ekt mokd une kiyl blnn resp ek kohomd blnna
-              var responseTextJSON = JSON.stringify( http.responseText)
+              var responseTextJSON = JSON.stringify( http.responseText) //responce ek gnnwa stringifymethod ek use krgnnwa java EE wal krpu ek wage jason krn ek //stringifymethod => awa resp ek jason ekkt convert krla gnn ek
                 console.log(responseTextJSON)
             }else {
-                console.error("Failed to statement")
+                console.error("Failed to statement") // resp ekt mokkhri unot ek balagnn
                 console.error("Status" + http.status)
                 console.error("Ready State" + http.readyState)
             }
@@ -81,9 +82,9 @@ function saveStudentData() {
 
         }
     } //02
-    http.open("POST","http://localhost:8080/StudentManagement/student",true) //03
-    http.setRequestHeader("Content-Type","application/json") //04
-    http.send(studentJSON) // empty body to coll GET method //05
+    http.open("POST","http://localhost:8080/StudentManagement/student",true) //03 true unot withryi refresh nowi ynne assintonice (url ekyi assintonix d sintonix d kiyl)
+    http.setRequestHeader("Content-Type","application/json") //04 Header- adishanal information ekk ywanne header walin === payload eke ewnne jason data ekk kiyl== denne
+    http.send(studentJSON) // empty body to coll GET method //05 //jason object ek yawanawa
 
 
     // CORS policy - domain dekk athule data shar kragnn giyam enne domain dekk athara data shar krnn be
